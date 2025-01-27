b9899537e622e351c4b6b4080f75532bb4e87189
syaro910 <ryuseifgo@gmail.com>
2025-01-27 14:27:51 +0900

    カート内の削除ボタン処理実装

---
+++ js/delete_cart.js
@@ -0,0 +1,26 @@
+//カート内商品の削除
+
+function delete_cart(cart_id){
+
+    //キャンセル
+    if(!confirm("本当に削除しますか？")){
+        return
+    }
+    
+	let delete_button = document.getElementById(cart_id);
+	let parent = delete_button.closest(".cart_box");
+	parent.remove();
+
+
+    //サーバーサイドに送る
+
+    let xhr = new XMLHttpRequest();
+
+    // //サーバーサイドに送るデータ
+    let req_data = `cart_id=${cart_id}`;
+
+    xhr.open('POST', '/pictsuba/api/****');
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.send(req_data);
+}
