33a53989476153db56ef47555eb8201784ac5707
syaro910 <ryuseifgo@gmail.com>
2025-01-27 11:32:59 +0900

    購入確認画面のご請求金額変更

--- js/calc_amount.js
+++ js/calc_amount.js
@@ -5,6 +5,19 @@ const details_amount_points = document.querySelectorAll(".details_amount_point")
 let use_point = document.getElementById("use_point");
 const is_use_point = document.getElementById("is_use_point");
 const point = document.getElementById("point");
+const show_product_total = document.getElementById("show_product_total");
+const product_total = document.getElementById("product_total");
+const total_amount = document.getElementById("total_amount");
+const show_postage =document.getElementById("show_postage");
+
+const postage = 500;
+
+window.onload = () => {
+	show_product_total.innerText = product_total.value;
+	show_postage.innerText = postage;
+
+	calc_amount(Number(product_total.value), Number(postage), Number(point.value));
+};
 
 is_use_point.addEventListener("change", ()=>{
 	if(is_use_point.checked){
@@ -12,6 +25,7 @@ is_use_point.addEventListener("change", ()=>{
 			details_amount_point.classList.add("view");
 		});
 
+		point.value = 0;
 		use_point.textContent = "0";
 	} else {
 		details_amount_points.forEach((details_amount_point)=>{
@@ -19,9 +33,15 @@ is_use_point.addEventListener("change", ()=>{
 		});
 
 		point.value = 0;
+		use_point.textContent = "0";
 	}
 })
 
 point.addEventListener("change", (event)=>{
 	use_point.textContent = "-" + point.value;
+	calc_amount(Number(product_total.value), Number(postage), Number(point.value));
 });
+
+function calc_amount(product_total, postage, point){
+	total_amount.innerText = product_total + postage - point;
+}
