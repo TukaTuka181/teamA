13ecbb177220c591725db56f65f7880c8aed6940
syaro910 <ryuseifgo@gmail.com>
2025-01-28 11:13:34 +0900

    削除ボタン処理と追加ボタン処理の実装

--- js/delete_address.js
+++ js/delete_address.js
@@ -1,21 +1,23 @@
 //住所情報の削除
-
 function delete_address(address_id){
 
-    //キャンセル
-    if(!confirm("本当に削除しますか？")){
-        return
-    }
-    
-    //サーバーサイドに送る
+	//キャンセル
+	if(!confirm("本当に削除しますか？")){
+		return
+	}
+	
+	let delete_button = document.getElementById(address_id);
+	let parent = delete_button.closest(".item");
+	parent.remove();
 
-    let xhr = new XMLHttpRequest();
+	// サーバーサイドに送る
+	let xhr = new XMLHttpRequest();
 
-    //サーバーサイドに送るデータ
-    let req_data = `address_id=${address_id}`;
+	//サーバーサイドに送るデータ
+	let req_data = `address_id=${address_id}`;
 
-    xhr.open('POST', '/pictsuba/api/****');
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.open('POST', '/pictsuba/api/****');
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    xhr.send(req_data);
+	xhr.send(req_data);
 }
