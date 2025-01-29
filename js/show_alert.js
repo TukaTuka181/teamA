db1ffdd0cd139245d2e2f88662de077742d6d981
syaro910 <ryuseifgo@gmail.com>
2025-01-29 18:44:48 +0900

    アラート表示の変更

--- js/show_alert.js
+++ js/show_alert.js
@@ -1,15 +1,26 @@
-//アラートの表示
-function show_alert(show_text){
-	
+let do_flag = 0;
+
+// アラートの表示
+function show_alert(show_text) {
+	if(do_flag === 1){
+		return
+	} else {
+		do_flag = 1;
+	}
+
 	let alert_window = document.getElementById("alert_window");
 	let alert_text = document.getElementById("alert_text");
+	let alert_border = document.getElementById("alert_border");
 
-	alert_window.classList.toggle("view");
+	alert_window.classList.add("view");
 	alert_text.innerText = show_text;
+	alert_border.classList.add("view");
 
 	setTimeout(() => {
-		alert_window.classList.toggle("view");
-	},2000);
+		alert_window.classList.remove("view");
+		alert_border.classList.remove("view");
+		do_flag = 0;
+	}, 2000);
 }
 
 window.show_alert = show_alert;
