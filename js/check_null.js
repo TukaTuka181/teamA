60c58d1d47a5b9a58877145220e5b94ed6402c89
TukaTuka181 <oibc35162@ojs.ac.jp>
2025-01-30 15:26:10 +0900

    Update check_null.js

--- js/check_null.js
+++ js/check_null.js
@@ -1,15 +1,7 @@
-function is_product_null(main_elem, null_elem){
-	main_elem.classList.remove("view");
-	null_elem.classList.add("view");
-}
-
-window.onload = () =>{
-	//お気に入り商品が一つもなかったら何もなことを表示する
-	let products = document.getElementById("products");
-	let is_null = document.getElementById("is_null");
-
-	if(products.childElementCount <= 0){
-		is_product_null(products, is_null);
+function is_product_null(main_elem, null_elem, limit){
+	if(main_elem.childElementCount <= limit){
+		main_elem.classList.remove("view");
+		null_elem.classList.add("view");
 	}
 }
 
