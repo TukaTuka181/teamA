fb4808c7bb4422821cfe7ba5cf6c3eac84e31d93
syaro910 <ryuseifgo@gmail.com>
2025-02-03 00:29:29 +0900

    住所情報ページに入力チェック

--- js/add_address.js
+++ js/add_address.js
@@ -4,32 +4,15 @@ const req_pref = document.getElementById("pref");
 const req_municipalities = document.getElementById("municipalities");
 const req_street = document.getElementById("street");
 const req_building = document.getElementById("building");
-const add_address = document.getElementById("add_address");
+const address_form = document.getElementById("address_form");
 
 //住所情報の追加
-add_address.addEventListener("click", () => {
+address_form.addEventListener("submit", () => {
 
-    let error_text = "";
-
-    //未入力チェック
-    if(req_zip_code.value === ""){
-        error_text = "郵便番号が未入力です。\n";
-    }
-    if(req_pref.value === ""){
-        error_text += "都道府県が未入力です。\n";
-    }
-    if(req_municipalities.value === ""){
-        error_text += "市区町村が未入力です。\n";
-    }
-    if(req_street.value === ""){
-        error_text += "番地が未入力です";
-    }
-    
-    if(error_text !== ""){
-        alert(error_text);
+    if(address_form_flag === 1){
         return;
     }
-
+    
     let xhr = new XMLHttpRequest();
 
     //サーバーサイドに送るデータ
@@ -59,8 +42,8 @@ add_address.addEventListener("click", () => {
             child.innerHTML = `
             <div class="item_info">
                 〒${req_zip_code.value.toString().substring(0, 3)}-${req_zip_code.value.toString().substring(3, 7)}<br>
-                ${req_pref.value} ${req_municipalities.value}<br>
-                ${req_street.value}${req_building.value}
+                    ${req_pref.value} ${req_municipalities.value} ${req_street.value}<br>
+                    ${req_building.value}
             </div>
             <button class="delete_button" id="${resp_id}" onclick="delete_address(${resp_id})">削除</button>
             `;
